b0aca51fddf2645ce8e9fb28f16b817b08038ded
Chaseh-code <chwc8889@gmail.com>
2022-05-04 00:19:48 -0500

    Adding attributes to the Threads

--- cpp_multithread.cpp
+++ cpp_multithread.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <pthread.h>
+#include <unistd.h>
 
 using namespace std;
 
-void *PrintHello(void *);
+//void *PrintHello(void *);
+void *wait(void *);
 
 #define NUM_THREADS 5
 
-struct thread_data {
+/*struct thread_data {
     int thread_id;
     char *message;
-};
+};*/
 
 int main() {
     pthread_t threads[NUM_THREADS];
-    struct thread_data td[NUM_THREADS];
+    pthread_attr_t attr;
     int rc;
+    void *status;
+
+    // Initialize and set thread joinable
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
     for(int i = 0; i < NUM_THREADS; i++){
         cout << "main(): creating thread, " << i << endl;
-        td[i].thread_id = i;
-        td[i].message = "This is a great message man. Real good testing going on here :)";
-        rc = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
+        rc = pthread_create(&threads[i], &attr, wait, (void *)i);
 
         if(rc){
         cout << "Error: unable to create thread, " << rc << endl;
@@ -30,15 +35,39 @@ int main() {
         }
     }
 
+    // free attribute and wait for the other threads
+    pthread_attr_destroy(&attr);
+    for(int i = 0; i < NUM_THREADS; i++){
+        rc = pthread_join(threads[i], &status);
+        if(rc){
+        cout << "Error: unable to join thread, " << rc << endl;
+        exit(-1);
+        }
+        cout << "Main: completed thread id: " << i;
+        cout << " exiting with status: " << status << endl;
+    }
+
+    cout << "Main: program exiting." << endl;
     pthread_exit(NULL);
     return 0;
 }
 
-void *PrintHello(void *threadarg){
+/*void *PrintHello(void *threadarg){
     struct thread_data *my_data;
     my_data = (struct thread_data *) threadarg;
 
     cout << "Thread ID: " << my_data->thread_id;
     cout << " Message: " << my_data->message << endl;
+    pthread_exit(NULL);
+}*/
+
+void *wait(void *t){
+    int i;
+    long tid;
+    tid = long(t);
+
+    sleep(1);
+    cout << "Sleepin in thread " << endl;
+    cout << "Thread with id: " << tid << " ...exiting " << endl;
     pthread_exit(NULL);
 }
